12a334a4f732d018b4715d12f1f297e7e5cea174
Jennie <122613260+JennieJSD@users.noreply.github.com>
2023-12-08 22:39:33 -0800

    Update index.js

--- server/index.js
+++ server/index.js
@@ -3,37 +3,28 @@ const cors = require("cors");
 const app = express();
 const path = require("path");
 require("dotenv").config();
-
+const { authMiddleware,errorHandler, } = require("./middlewares/authMiddleware");
 const dbConnect = require("./config/dbConnect");
 dbConnect();
+
+
+
 
 
 app.use(cors());
 app.use(express.json());
 
-
-
 // Routes import
 const userRoutes = require("./routes/userRoutes");
 
+const productRoutes = require("./routes/productRoutes");
+const taskRoutes = require("./routes/taskRoutes");
 
 
-
-
+// Define your routes
 app.use("/api/user", userRoutes);
-
-
-
-
-
-
-
-
-
-
-
-
-
+app.use("/api/product", productRoutes);
+app.use("/api/task", taskRoutes);
 
 
 
