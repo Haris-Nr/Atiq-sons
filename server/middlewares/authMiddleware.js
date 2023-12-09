835a675ab75d597b101fc0c8cfba8604a6447be3
Jennie <122613260+JennieJSD@users.noreply.github.com>
2023-12-08 22:40:14 -0800

    Update authMiddleware.js

--- server/middlewares/authMiddleware.js
+++ server/middlewares/authMiddleware.js
@@ -1,9 +1,9 @@
 const jwt = require("jsonwebtoken");
 
-module.exports = (req, res, next) => {
+const authMiddleware = (req, res, next) => {
     try {
         const token = req.header("Authorization");
-        
+
         if (!token) {
             return res.status(401).json({
                 success: false,
@@ -11,8 +11,15 @@ module.exports = (req, res, next) => {
             });
         }
 
-        const decryptedToken = jwt.verify(token, process.env.JWT_SECRET);
-        req.body.userId = decryptedToken.userId;
+        const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = {
+            userId: decodedToken.userId,
+            // You can include other user information here if needed
+        };
+        
+        // Now req.user contains the user information
+        console.log('req.user:', req.user);
+
         next();
     } catch (error) {
         res.status(401).json({
@@ -20,4 +27,21 @@ module.exports = (req, res, next) => {
             message: error.message,
         });
     }
+};
+
+const errorHandler = (err, req, res, next) => {
+    console.error(err);
+  
+    const statusCode = err.statusCode || 500;
+    res.status(statusCode).json({
+        error: {
+            status: statusCode,
+            message: err.message,
+        },
+    });
+};
+
+module.exports = {
+    authMiddleware,
+    errorHandler,
 };
