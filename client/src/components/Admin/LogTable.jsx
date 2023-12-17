6f891cc5a475eb73147e4685f965333a3c27e791
Jennie <122613260+JennieJSD@users.noreply.github.com>
2023-12-17 06:38:16 -0800

    Update LogTable.jsx

--- client/src/components/Admin/LogTable.jsx
+++ client/src/components/Admin/LogTable.jsx
@@ -8,24 +8,34 @@ const  LogTable  = () => {
     {
       title: "ID",
       dataIndex: "id",
+      className: 'text-md  tracking-wide text-left text-gray-900 bg-gray-100 uppercase border-b border-gray-600 cursor-pointer',
+
     },
     {
       title: "Name",
       dataIndex: "Name",
+      className: 'text-md  tracking-wide text-left text-gray-900 bg-gray-100 uppercase border-b border-gray-600 cursor-pointer',
+
       render: (value) => <span>{value}</span>,
     },
     {
       title: "Dashboard",
       dataIndex: "dashboard",
+      className: 'text-md  tracking-wide text-left text-gray-900 bg-gray-100 uppercase border-b border-gray-600 cursor-pointer',
+
       render: (value) => <span>{value}</span>,
     },
     {
       title: "Login Time",
       dataIndex: "login",
+      className: 'text-md  tracking-wide text-left text-gray-900 bg-gray-100 uppercase border-b border-gray-600 cursor-pointer',
+
     },
     {
       title: "LogOut Time",
       dataIndex: "logout",
+      className: 'text-md  tracking-wide text-left text-gray-900 bg-gray-100 uppercase border-b border-gray-600 cursor-pointer',
+
     },
   ]
 
