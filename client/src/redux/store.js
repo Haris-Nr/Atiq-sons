5dfa3bcdc623516d478e9bcf4d67fc0b89b7be1b
Bilal Abbas <141160824+bilal7472@users.noreply.github.com>
2023-12-21 13:50:46 +0000

    Add files via upload

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -3,12 +3,14 @@ import authReducer from './Features/auth/authSlice'
 import fetchReducer from './Features/auth/fetchSlice'
 import employeeReducer from './Features/Employees/employeeSlice'
 import productReducer from './Features/Product/productSlice'
+import logReducer from './Features/auth/logSlice'
 
 export const store = configureStore({
   reducer: {
     auth:authReducer,
     fetch:fetchReducer,
+    log:logReducer,
     employee:employeeReducer,
     product:productReducer,
   },
-})
+});
