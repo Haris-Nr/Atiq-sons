6049b80c2ff1790e9df2f0b4cd49d9201f202dd7
Bilal Abbas <141160824+bilal7472@users.noreply.github.com>
2023-12-24 04:33:38 +0000

    productapi

--- client/src/redux/api/productApi.js
+++ client/src/redux/api/productApi.js
@@ -2,23 +2,69 @@ import axiosInstance from './axiosInstance';
 
 const addProduct = async (productData) => {
     try {
-        const response = await axiosInstance.post(`/product/addproduct`, productData,{
+        const response = await axiosInstance.post(`/product/addproduct`, productData, {
             headers: {
                 'Content-Type': 'multipart/form-data'
             }
         });
-        return response.data;  
+        return response.data;
     } catch (error) {
-        throw error.response.data 
+        throw error.response.data
     }
 };
 
 const fetchProductsbyemployee = async (productData) => {
     try {
-        const response = await axiosInstance.post(`/product/fetchProductsbyemployee`,{createdBy:productData});
-        return response.data;  
+        const response = await axiosInstance.post(`/product/fetchProductsbyemployee`, { createdBy: productData });
+        return response.data;
     } catch (error) {
-        throw error.response.data 
+        throw error.response.data
+    }
+};
+
+const getSingleProduct = async (id) => {
+    try {
+        const response = await axiosInstance.get(`/product/fetchSingleProduct/${id}`);
+        return response.data;
+    } catch (error) {
+        throw error.response.data;
+    }
+
+};
+
+const getAllProduct = async () => {
+    try {
+        const response = await axiosInstance.get(`/product/fetchallProduct`);
+        return response.data;
+    } catch (error) {
+        throw error.response.data;
+    }
+};
+
+const deleteProduct = async (id) => {
+    try {
+        const response = await axiosInstance.delete(`/product/deleteproduct/${id}`);
+        return response.data;
+    } catch (error) {
+        throw error.response.data;
+    }
+};
+
+const updateProduct = async (id) => {
+    try {
+        const response = await axiosInstance.patch(`/product/updateProduct/${id}`);
+        return response.data;
+    } catch (error) {
+        throw error.response.data;
+    }
+};
+
+const changeStatus = async (id, newStatus) => {
+    try {
+        const response = await axiosInstance.patch(`/product/changeProductStatus/${id}`, { status: newStatus });
+        return response.data;
+    } catch (error) {
+        throw error.response.data;
     }
 };
 
@@ -26,7 +72,12 @@ const fetchProductsbyemployee = async (productData) => {
 
 const productApi = {
     addProduct,
-    fetchProductsbyemployee
+    fetchProductsbyemployee,
+    getSingleProduct,
+    getAllProduct,
+    deleteProduct,
+    changeStatus,
+    updateProduct,
 };
 
 export default productApi;
